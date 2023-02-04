b5b6c9ac029531c5d80fee769ec62ea110f18db4
sweethappydream <ethanmartin.0508@gmail.com>
2023-02-05 05:10:30 +1300

    sidebar crusor change

--- src/components/Sidebar/SingleTab.jsx
+++ src/components/Sidebar/SingleTab.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SigleTab = (props) => {
     const { src, srcHover, name } = props;
     return (
-        <div className="group w-[85px] h-[54px] p-8 flex flex-col items-center justify-center  hover:bg-hover">
+        <div className="group w-[85px] h-[54px] p-8 flex flex-col items-center justify-center cursor-pointer hover:bg-hover">
             <img src={src} alt="" className="pb-[6px] group-hover:hidden"/>
             <img src={srcHover} alt="" className="pb-[6px] hidden group-hover:block"/>
             <p className=" group-hover:text-white">{name}</p>
