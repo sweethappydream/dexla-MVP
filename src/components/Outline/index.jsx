8da8cdfaffabb4c41201fff981b66feadf38bf8a
sweethappydream <ethanmartin.0508@gmail.com>
2023-02-06 20:33:55 +1300

    pane hidden in preview mode

--- src/components/Outline/index.jsx
+++ src/components/Outline/index.jsx
@@ -1,10 +1,19 @@
-import React, {useState} from "react";
+import React, {useState, useContext, useEffect} from "react";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
 import SubBar from "../Sidebar/SubBar";
+import { editContext } from "../../context";
 
 const Outline = ({children}) => {
     const [isOpen, setIsOpen] = useState(false);
+
+    const { isEditable } = useContext(editContext);
+    
+    useEffect(() => {
+        if(!isEditable)
+            setIsOpen(false);
+    },[isEditable])
+
     return (
         <>
             <Header/>
